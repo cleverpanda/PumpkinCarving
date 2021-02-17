9ef87fbfc896146c0fc66b989d73af428a71c8b3
johnmeshulam <johnmeshulam@gmail.com>
2021-02-17 19:03:55 +0200

    formatting changes

--- src/main/java/panda/pumpkincarving/ItemBlockPumpkin.java
+++ src/main/java/panda/pumpkincarving/ItemBlockPumpkin.java
@@ -17,50 +17,47 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemBlockPumpkin extends ItemBlock{
+public class ItemBlockPumpkin extends ItemBlock {
 
-	
 	@Override
-	 public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, IBlockState newState)
-    {
-        if (!world.setBlockState(pos, newState, 11)) return false;
+	public boolean placeBlockAt(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side,
+			float hitX, float hitY, float hitZ, IBlockState newState) {
+		if (!world.setBlockState(pos, newState, 11))
+			return false;
 
-        IBlockState state = world.getBlockState(pos);
-        if (state.getBlock() == this.block)
-        {
-            setTileEntityNBT(world, player, pos, stack);
-            this.block.onBlockPlacedBy(world, pos, state, player, stack);
+		IBlockState state = world.getBlockState(pos);
+		if (state.getBlock() == this.block) {
+			setTileEntityNBT(world, player, pos, stack);
+			this.block.onBlockPlacedBy(world, pos, state, player, stack);
 
-            if (player instanceof EntityPlayerMP)
-                CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP)player, pos, stack);
-        }
+			if (player instanceof EntityPlayerMP)
+				CriteriaTriggers.PLACED_BLOCK.trigger((EntityPlayerMP) player, pos, stack);
+		}
 
-        return true;
-    }
-	
+		return true;
+	}
+
 	public ItemBlockPumpkin(Block block) {
 		super(block);
 		setHasSubtypes(true);
 		setMaxDamage(0);
 	}
 
-    @Override
-    public int getMetadata(int damage)
-    {
-        return damage;
-    }
-    
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> list)
-    {
-    	if(tab != CreativeTabs.DECORATIONS)
-    		return;
-    	list.add(new ItemStack(this, 1, 0));
-    	list.add(new ItemStack(this, 1, 1));
-    	list.add(new ItemStack(this, 1, 2));
-    	list.add(new ItemStack(this, 1, 3));
-    }
+	@Override
+	public int getMetadata(int damage) {
+		return damage;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> list) {
+		if (tab != CreativeTabs.DECORATIONS)
+			return;
+		list.add(new ItemStack(this, 1, 0));
+		list.add(new ItemStack(this, 1, 1));
+		list.add(new ItemStack(this, 1, 2));
+		list.add(new ItemStack(this, 1, 3));
+	}
 
 	@Override
 	public boolean isValidArmor(ItemStack stack, EntityEquipmentSlot armorType, Entity entity) {
